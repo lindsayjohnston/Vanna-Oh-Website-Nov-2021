6c00184be3f21bc6ea93301597c0916b91753f60
Lindsay Johnston <lindsaykjohnston@outlook.com>
2021-11-26 15:20:35 -0800

    Create React.  Replace React branding. Set up module folders

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div class="mainBodyMobile">
+      this is the body
+
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
