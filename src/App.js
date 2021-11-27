ac192d2e498e05ebb9984da55b4a8dcda064b247
Lindsay Johnston <lindsaykjohnston@outlook.com>
2021-11-27 13:01:00 -0800

    Added screen orientation listener to App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
+import React from 'react';
 import './App.css';
 import MainBodyDesktop from "./components/desktop/MainBodyDesktop/MainBodyDesktop.js";
 import MainBodyMobile from './components/mobile/MainBodyMobile/MainBodyMobile';
 
 
-let screenOrientation= window.screen.orientation;
-console.log(screenOrientation); 
+const screen= window.screen;
 
-function App() {
-  
 
-  return (
-    <div className="App">
+class App extends React.Component{
+
+state = {
+  screenOrientation: window.screen.orientation
+}
+
+constructor(props) {
+  super(props);
+  this.setScreenOrientation = this.setScreenOrientation.bind(this);
+}
+
+setScreenOrientation = () =>{
+  this.setState({
+    screenOrientation: window.screen.orientation.type
+  })
+
+  console.log("set screen orientation. State: " + this.state.screenOrientation);
+}
+
+componentDidMount(){
+  // this.setScreenOrientation();
+  screen.orientation.addEventListener("change", this.setScreenOrientation);
+}
+
+
+render() {
+
+  return(
+    <div className= "App">
       <MainBodyDesktop className="bodyDesktop" />
       <MainBodyMobile className="bodyMobile" screen= "mobile" />
 
-
     </div>
-  );
+
+  )
+} 
 }
 
 export default App;
