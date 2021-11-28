be83c753302f3a1edd1ab82a1c764d3f2721a71d
Lindsay Johnston <lindsaykjohnston@outlook.com>
2021-11-27 16:30:47 -0800

    Added media rule for mobile screens - different background image

--- src/components/portrait/MainBodyPortrait/MainBodyPortrait.js
+++ src/components/portrait/MainBodyPortrait/MainBodyPortrait.js
@@ -1,15 +1,11 @@
 import './MainBodyPortrait.css';
 
 const mainBodyPortrait = (props) => {
-    //function go here
 
     return (
         <div className="mainBodyPortrait">
-            
         </div>
-
     )
-
 }
 
 export default mainBodyPortrait;
