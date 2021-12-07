ac9f38bf22a0b836b722ea9c32fa7a6644413272
Lindsay Johnston <lindsaykjohnston@outlook.com>
2021-12-06 18:48:06 -0800

    Remembered how to add onClick to fire an event. Added flipCardHandler to all the Landscape buttons

--- src/components/landscape/MenuLandscape/MenuLandscape.js
+++ src/components/landscape/MenuLandscape/MenuLandscape.js
@@ -3,10 +3,20 @@ import './MenuLandscape.css';
 const menuLandscape = (props) =>{
     const pagesInfo= props.pages;
     const pagesButtons = pagesInfo.map((pageInfo) => 
-        <button className="buttonLandscape" key={pageInfo.id}>
+        <button className="buttonLandscape" 
+            id={pageInfo.id} 
+            key={pageInfo.id} 
+            onClick={() => flipCardHandler(pageInfo.id, pageInfo)}>
             {pageInfo.title}
         </button>
     );
+
+    
+    function flipCardHandler(id, pageInfoArray){
+        alert( id + " clicked");
+       
+    }
+    
 
     return (
         <div className="menuLandscape">
