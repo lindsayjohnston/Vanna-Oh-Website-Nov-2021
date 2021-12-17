ea1d86576cdaca979feb901ae8d1527a74021e64
Lindsay Johnston <lindsaykjohnston@outlook.com>
2021-12-16 16:36:21 -0800

    Fixed layout of the title, menu, and content areas of the Square layout

--- src/components/square/MainBodySquare/MainBodySquare.js
+++ src/components/square/MainBodySquare/MainBodySquare.js
@@ -3,13 +3,13 @@ import MenuSquare from '../MenuSquare/MenuSquare';
 import albumCover from '../../../media/sad-mode-album-cover-1024x1024.jpg'
 
 const mainBodySquare = (props) => {
-    let imgContainerClasses= "imgContainer flipCard";
-    let backButtonClasses= "titleSquare backButton";
+    let imgContainerClasses= "imgContainerSquare flipCardSquare";
+    let backButtonClasses= "titleSquare backButtonSquare";
     let contentOrLinks= null;
 
     if (props.pageShown === "home"){
         backButtonClasses += " hidden";
-        imgContainerClasses = "imgContainer";
+        imgContainerClasses = "imgContainerSquare";
     } else {
         const pageIndex= props.pageShown - 1;
         const pageInfo= props.pages[pageIndex];
@@ -24,28 +24,32 @@ const mainBodySquare = (props) => {
 
     return (
         <div className='mainBodySquare'>
-            <div className="titleContainerSquare">
-                <h1 className="titleSquare"> vanna oh! </h1>
-                <h1 className={backButtonClasses}
-                    onClick={props.backButtonHandler}>
-                    back</h1>
-            </div>
+            
             
             <div className="squareContentContainer">
+                <div className="titleContainerSquare">
+                    <h1 className="titleSquare"> vanna oh! </h1>
+                    <h1 className={backButtonClasses}
+                        onClick={props.backButtonHandler}>
+                        back</h1>
+                </div>
+
                 <div className={imgContainerClasses}>
-                    <div className="front">
+                    <div className="frontSquare">
                         <img src={albumCover} className="albumCoverSquare" alt="sad-mode-album-cover"></img>
                     </div>
-                    <div className="back">
+                    <div className="backSquare">
                         {contentOrLinks}
                     </div>
                 </div>
 
-            </div>
-          
-            <MenuSquare menuClick = {props.menuClick}
+                <MenuSquare menuClick={props.menuClick}
                     pageShown={props.pageShown}
                     pages={props.pages} />
+
+            </div>
+          
+            
         </div>
     )
 }
