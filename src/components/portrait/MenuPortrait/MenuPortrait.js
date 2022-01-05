d89c5b09710db875c8341dfcc51c9151b94d6aaf
Lindsay Johnston <lindsaykjohnston@outlook.com>
2022-01-05 08:58:05 -0800

    Hid both menu containers for testing on mobile

--- src/components/portrait/MenuPortrait/MenuPortrait.js
+++ src/components/portrait/MenuPortrait/MenuPortrait.js
@@ -7,8 +7,8 @@ const menuPortrait = (props) => {
     let menuContainerClasses= "hidden";
     let pageTitleContainerClasses="hidden";
     if(props.pageShown === "home"){
-        menuContainerClasses= "menuContainerPortrait fadeIn";
-        pageTitleContainerClasses ="hidden fadeOut"
+        // menuContainerClasses= "menuContainerPortrait fadeIn";
+        // pageTitleContainerClasses ="hidden fadeOut"
         //generate all the buttons for the menu
        pagesButtons = pagesInfo.map((pageInfo) => 
         <button className="buttonPortrait" 
@@ -21,8 +21,8 @@ const menuPortrait = (props) => {
     } else {
         //generate one disabled 'button' for the "pageShown"
         // menuContainerClasses= "hidden";
-        menuContainerClasses= "hidden fadeOut";
-        pageTitleContainerClasses ="menuContainerPortrait fadeIn"
+        // menuContainerClasses= "hidden fadeOut";
+        // pageTitleContainerClasses ="menuContainerPortrait fadeIn"
         let pageInfo= props.pages[(pageShown - 1)];
         pagesButtons = <button 
         disabled
