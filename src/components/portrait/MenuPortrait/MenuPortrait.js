514df9a698a0709f9f13874685c6ecbde7643b52
Lindsay Johnston <lindsaykjohnston@outlook.com>
2022-01-05 08:47:28 -0800

    Added fadeIn fadeOut classes

--- src/components/portrait/MenuPortrait/MenuPortrait.js
+++ src/components/portrait/MenuPortrait/MenuPortrait.js
@@ -7,7 +7,8 @@ const menuPortrait = (props) => {
     let menuContainerClasses= "hidden";
     let pageTitleContainerClasses="hidden";
     if(props.pageShown === "home"){
-        menuContainerClasses= "menuContainerPortrait";
+        menuContainerClasses= "menuContainerPortrait fadeIn";
+        pageTitleContainerClasses ="hidden fadeOut"
         //generate all the buttons for the menu
        pagesButtons = pagesInfo.map((pageInfo) => 
         <button className="buttonPortrait" 
@@ -20,7 +21,8 @@ const menuPortrait = (props) => {
     } else {
         //generate one disabled 'button' for the "pageShown"
         // menuContainerClasses= "hidden";
-        pageTitleContainerClasses = "menuContainerPortrait"
+        menuContainerClasses= "hidden fadeOut";
+        pageTitleContainerClasses ="menuContainerPortrait fadeIn"
         let pageInfo= props.pages[(pageShown - 1)];
         pagesButtons = <button 
         disabled
